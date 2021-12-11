fe3de9c248f2f2e1c07128878bff4f7e8b61ec00
orion <orion@orionc.dev>
2021-12-10 21:31:51 -0800

    cleaned up formatting

--- api.py
+++ api.py
@@ -11,13 +11,12 @@ import config
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-def get_access(check = 0):
 
+def get_access():
   auth = SpotifyClientCredentials(
     client_id=config.client_id,
     client_secret=config.client_secret)
   sp = spotipy.Spotify(auth_manager = auth)
-
   return sp
 
 
@@ -25,9 +24,8 @@ def get_artist(url):
   spotify = get_access()
   return spotify.artist_albums(artist_id=url, limit=50)
 
+
 def get_playlist(url):
   spotify = get_access()
   return spotify.playlist(url, fields='name,tracks.items.track.album.name,tracks.items.track.album.images', market='US')
 
-if __name__ == '__main__':
-  get_access(1)
