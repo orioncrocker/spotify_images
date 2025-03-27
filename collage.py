cc97b7d880db5d3090c6ae5726bdc04f95f8019c
Orion <orion@orionc.dev>
2025-03-27 00:27:04 -0700

    Finish wallpaper feature

--- collage.py
+++ collage.py
@@ -15,9 +15,10 @@ import globals
 from PIL import Image
 
 ratio_by_resolution = {
-    "4:3": [(640,480),(800,600),(1024,768),(1280,960)],
-    "16:9": [(1280,720),(1920,1080),(2048,1152),(4096,2304)],
-    "16:10": [(1280,800),(1440,900),(1680,1050,(3840,2440))],
+    "4:3":   [(800,600),(1024,768),(1280,960)],
+    "16:9":  [(1280,720),(1600,900),(1920,1080),(2560,1440),(3840,2160),(5120,2880),(7680,4320)],
+    "16:10": [(1280,800),(1440,900),(1680,1050),(1920,1200),(2560,1600),(3840,2400)],
+    "32:9":  [(3840,1080),(5120,1400),(6400,1800),(7680,2160)],
 }
 
 
@@ -40,23 +41,6 @@ def get_closest_resolution(ratio, x_size, y_size):
   closest_y = resolutions[idx][1]
   return closest_x, closest_y
 
-def calc_grid(total, square=False):
-  # Keep it simple for results with few images
-  if total < 15 or square:
-    grid_size = math.sqrt(total)
-    if grid_size.is_integer():
-      rows, cols = (int(grid_size), int(grid_size))
-    else:
-      rows, cols = (math.floor(grid_size), math.ceil(grid_size))
-      if (rows * cols) > total:
-        cols -= 1
-    return rows, cols
-
-  rows = 0
-  cols = 0
-
-  return (rows, cols)
-
 def get_wallpaper_ratio():
   x,y = map(int, globals.wallpaper_ratio.split(':'))
   return x/y
@@ -66,7 +50,6 @@ def calc_wallpaper_border(rows, cols):
   current_ratio = cols/rows
   if globals.verbose:
     print("Target ratio: %f" % target_ratio)
-    print("Current ratio: %f" % current_ratio)
 
   x_size = globals.image_size * cols
   x_needed = ((x_size * target_ratio) / current_ratio) - x_size
@@ -76,16 +59,58 @@ def calc_wallpaper_border(rows, cols):
   y_border = 0
   return x_border,y_border
 
+def calc_wallpaper_grid(total):
+  wallpaper_ratio = get_wallpaper_ratio()
+
+  square = math.ceil(math.sqrt(total))
+  closest_ratio = square/square
+  final_cols = square
+  final_rows = square
+
+  for cols in range(square, square*2):
+    rows = square - (cols - square)
+    ratio = cols/rows
+    wall_diff = wallpaper_ratio - ratio
+
+    if wall_diff < 0:
+        closest_ratio = ratio
+        final_cols = cols
+        final_rows = rows
+
+        if total < (final_cols * final_rows):
+          final_cols -= 1
+        break;
+
+  return final_rows, final_cols
+
+def calc_collage_grid(total):
+  grid_size = math.sqrt(total)
+  if grid_size.is_integer():
+    rows, cols = (int(grid_size), int(grid_size))
+  else:
+    rows, cols = (math.floor(grid_size), math.ceil(grid_size))
+    if (rows * cols) > total:
+      cols -= 1
+  return (rows, cols)
+
 def make_collage(directory, wallpaper):
+  product = "wallpaper" if wallpaper else "collage"
+
   pics = []
   if directory:
-    pics = glob.glob(directory + '/*.jpeg')
+    pics = glob.glob(directory + '/*' + globals.image_ext)
   pics = [Image.open(i) for i in pics]
 
   if globals.verbose:
-    print("Total unique pictures: " + str(len(pics)))
+    print("Total unique images found in directory: %d" % (len(pics)))
 
-  rows,cols = calc_grid(len(pics))
+  if wallpaper:
+    rows,cols = calc_wallpaper_grid(len(pics))
+  else:
+    rows,cols = calc_collage_grid(len(pics))
+
+  print("Creating %s using %d images from %s" %
+        (product, rows*cols, directory))
 
   if globals.verbose:
     print("Rows: %d Cols: %d" % (rows, cols))
@@ -135,7 +160,7 @@ def make_collage(directory, wallpaper):
     filename = (directory + globals.image_ext)
 
   collage.save(filename)
-  print("Collage saved as: " + filename)
+  print("%s saved as: %s" % (product, filename))
 
 
 def test():
